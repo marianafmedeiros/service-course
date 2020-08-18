c6edb6be76bd7ec6f78b31e17884e8bb9537e784
Mariana <mariana.fmedeiros@outlook.com>
2020-08-18 10:56:22 -0300

    add analytics client

--- node/handlers/analytics.ts
+++ node/handlers/analytics.ts
@@ -1,9 +1,8 @@
 export async function analytics(ctx: Context, next: () => Promise<any>) {
-  if (ctx.method.toUpperCase() === 'GET') {
-    ctx.status = 200
-    ctx.body = 'OK'
-
-    ctx.set('cache-control', 'no-cache')
-  }
+  const { clients: { analytics }, } = ctx
+  ctx.status = 200
+  ctx.body = await analytics.getLiveUsers()
+  ctx.set('cache-control', 'no-cache')
   await next()
+
 }
