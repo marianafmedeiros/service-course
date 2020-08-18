6b4e8017acc819c74a266a913de15b3dfa0320e5
Mariana <mariana.fmedeiros@outlook.com>
2020-08-18 11:39:17 -0300

    fetch and update products

--- node/event/liveUsersUpdate.ts
+++ node/event/liveUsersUpdate.ts
@@ -1,3 +1,41 @@
-export async function updateLiveUsers() {
-  console.log('EVENT HANDLER: received event')
+import { Clients } from '../clients/index'
+import { EventContext } from '@vtex/api'
+import { COURSE_ENTITY } from '../utils/constants'
+
+export async function updateLiveUsers(ctx: EventContext<Clients>) {
+ const liveUsersProducts = await ctx.clients.analytics.getLiveUsers()
+ console.log('LIVE USERS:', liveUsersProducts)
+ await Promise.all (
+   liveUsersProducts.map(async ({ slug, liveUsers }) => {
+     try{
+       const [savedProduct] = await ctx.clients.masterdata.searchDocuments<{
+         id: string,
+         count: number,
+         slug: string
+       }> ({
+         dataEntity: COURSE_ENTITY,
+         fields: ['count', 'id', 'slug'],
+         pagination: {
+           page: 1,
+           pageSize: 1,
+         },
+         schema: 'v1',
+         where: `slug=${slug}`
+       })
+       console.log('SAVED PRODUCT', savedProduct)
+       await ctx.clients.masterdata.createOrUpdateEntireDocument({
+         dataEntity: COURSE_ENTITY,
+         fields: {
+           count: liveUsers,
+           slug,
+         },
+         id: savedProduct?.id,
+       })
+     } catch (e) {
+       console.log(`failed to update product ${slug}`)
+       console.log(e)
+     }
+   })
+ )
+ return true 
 }
